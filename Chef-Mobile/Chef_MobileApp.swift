f2b7f1911957dc50f0c499c5eb19250d93bb1936
AdnaneAbs <elabbasadnane@gmail.com>
2022-02-14 18:41:45 +0100

    Initial Commit

---
+++ Chef-Mobile/Chef_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chef_MobileApp.swift
+//  Chef-Mobile
+//
+//  Created by Adnane El Abbas on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Chef_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
